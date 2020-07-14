a285e8b842a92f287c4c078e56d631f08c71aa20
enea <enea.so@live.com>
2020-07-13 20:37:24 -0500

    feat: add helpers for index and filters

---
+++ src/WithFilters.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by enea dhack - 12/06/2020 17:07.
+ */
+
+namespace Vaened\Searcher;
+
+use Vaened\Searcher\Filterable;
+
+trait WithFilters
+{
+    abstract protected function getFilterProvider(): Filterable;
+
+    public function filter(array $filters): self
+    {
+        $constraints = $this->getFilterProvider()->getConstraints($filters);
+        foreach ($constraints as $constraint) {
+            $this->apply($constraint);
+        }
+        return $this;
+    }
+}
---
+++ src/WithIndexes.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by enea dhack - 13/06/2020 13:01.
+ */
+
+namespace Vaened\Searcher;
+
+use Vaened\Searcher\Indexable;
+
+trait WithIndexes
+{
+    abstract protected function getIndexProvider(): Indexable;
+
+    public function search(string $index, ?string $q): self
+    {
+        if ($q != null) {
+            $this->apply($this->getIndexProvider()->getConstraint($index, $q));
+        }
+
+        return $this;
+    }
+
+}
